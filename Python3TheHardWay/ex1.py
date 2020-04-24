89f3ee99befbbf54dc27560b8dc9364047c80984
Daniel Savage <dansavag@cisco.com>
2020-04-24 13:42:39 +0100

    Trail stone stuff

---
+++ Python3TheHardWay/ex1.py
@@ -0,0 +1,7 @@
+def break_words(stuff):
+    return stuff.split('n')
+
+def sorted_words(t):
+    return t.sort()
+v = break_words("testing innit")
+print(sorted_words(v))
---
+++ Python3TheHardWay/loops.py
@@ -0,0 +1,11 @@
+import math
+for letter in "python":
+    print(letter)
+
+string = "python"
+for i in range(len(string)):
+    if (i % 2 == 0):
+        print(string[i])
+        print(' ')
+
+print(math.pi)
